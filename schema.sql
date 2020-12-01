fa0d4de85aac0a53d63b79ef438b7c33e9a7cba7
Lohelani <lohelani.hicks@gmail.com>
2020-11-30 22:35:35 -0500

    updated db

--- schema.sql
+++ schema.sql
@@ -4,28 +4,26 @@ CREATE database employeeTracker_DB;
 USE employeeTracker_DB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30),
-
-);
-
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+  );
+  
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
-  PRIMARY KEY (department_id),
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id VARCHAR (30),
-  PRIMARY KEY (position),
   FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
 SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
